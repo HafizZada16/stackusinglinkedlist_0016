e11b80c4c9639afa23131d1b0a7c4f7ce6df41e4
HafizZada16 <hafizzada16@gmail.com>
2024-05-11 10:02:59 +0700

    Add switch case

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -69,6 +69,35 @@ int main() {
         cout << "5. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter a value to push: ";
+                cin >> value;
+                stack.push(value);
+                break;
+            case 2:
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+                else {
+                    cout << "Stack is empty, cannot pop." << endl;
+                }
+                break;
+            case 3:
+                if (stack.isEmpty()) {
+                    stack.peek();
+                }
+                else {
+                    cout << "Stack is empty. No top value." << endl;
+                }
+                break;
+            case 4:
+                cout << "Exiting program." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Try again." << endl;
+        }
     }
 }
 
